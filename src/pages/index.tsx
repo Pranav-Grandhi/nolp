830ae8d0427701df73f9f987f6162c8b303fefb2
Agastya Gaur <agastya.gaur@gmail.com>
2021-12-02 21:58:13 +0000

    Images added to slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <HeroSection
-        children={<BackgroundSlider images={["", ""]} />}
+        children= {<BackgroundSlider images={["images/clothesShop.jpg", "images/coffeeShop.jpg", "images/coffeeShop2.jpg", "images/handicraftShop.jpg"]} duration={8} transition={2} />}
         title="Seperating the rest from the best"
         subtitle="Discover where you shouldn't go next"
       />
