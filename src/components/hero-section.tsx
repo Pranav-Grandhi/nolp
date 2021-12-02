01bd3106279f2363de22b1c9486e48c0cc818304
Pranav-Grandhi <pranav@grandhi.net>
2021-12-02 23:12:37 +0000

    fix background slider overflow

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -7,12 +7,12 @@ interface HeroSectionProps {
 export default function HeroSection({ children, title, subtitle }: HeroSectionProps) {
   return (
     <>
-      {children}
       <div className="relative pt-16 md:pt-24 lg:pt-32 pb-10 md:pb-16 lg:pb-24">
-        <div className="container mx-auto max-w-screen-xl px-4 sm:px-8 lg:px-12 xl:px-16 text-center">
-          <h1 className="text-2xl md:text-4xl font-bold text-gray-800 sm:text-3xl ">{title}</h1>
-          <p className="mt-3 text-gray-600 md:text-lg">{subtitle}</p>
-        </div>       
+        {children}
+        <div className="relative z-20 container mx-auto max-w-screen-xl px-4 sm:px-8 lg:px-12 xl:px-16 text-center">
+          <h1 className="text-2xl md:text-4xl font-bold text-white sm:text-3xl ">{title}</h1>
+          <p className="mt-3 text-white md:text-lg">{subtitle}</p>
+        </div>
       </div>
     </>
   )
