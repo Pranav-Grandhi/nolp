8ca83c3503b7c72d328ecfb6e9507f14d3658d43
Pranav-Grandhi <pranav@grandhi.net>
2021-12-09 19:02:45 +0000

    Fix 404 styles

---
+++ src/pages/404.tsx
@@ -0,0 +1,40 @@
+import HeroSection from 'components/hero-section'
+import Link from 'next/link'
+import { signIn } from 'next-auth/react'
+
+export default function DNE() {
+  return (
+    <>
+      <HeroSection
+        children={
+          <>
+            <div className="absolute w-full h-full bg-black top-0 left-0 bg-opacity-75"></div>
+            <img
+              src="images/static.gif"
+              alt="static"
+              className="w-full h-full top-0 left-0 absolute bg-cover bg-center bg-no-repeat"
+              style={{ zIndex: '-1000' }}
+            />
+          </>
+        }
+        title="Sorry!"
+        subtitle="The page you requested does not exist"
+      />
+      <section className="section">
+        <div className="w-full px-4 mx-3 sm:w-96">
+          <div className="grid grid-cols-1 gap-2">
+            <Link href="/">
+              <a className="form_red_button">Go home</a>
+            </Link>
+            <button
+              className="form_red_button"
+              onClick={() => signIn('google')}
+            >
+              Sign in/up
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
