99a709e483294a5549ff3ada7cffe7d50e78fcad
Agastya Gaur <agastya.gaur@gmail.com>
2021-12-09 20:13:11 +0000

    Icon property added to HeroSection

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -2,10 +2,12 @@ interface HeroSectionProps {
   title: string
   subtitle: string
   children?: any
+  icon?: any
 }
 
 export default function HeroSection({
   children,
+  icon,
   title,
   subtitle,
 }: HeroSectionProps) {
@@ -14,6 +16,7 @@ export default function HeroSection({
       <div className="relative pt-16 md:pt-24 lg:pt-32 pb-10 md:pb-16 lg:pb-24">
         {children}
         <div className="relative z-20 container mx-auto max-w-screen-xl px-4 sm:px-8 lg:px-12 xl:px-16 text-center">
+          {icon}
           <h1 className="text-2xl md:text-4xl font-bold text-white sm:text-3xl ">
             {title}
           </h1>
