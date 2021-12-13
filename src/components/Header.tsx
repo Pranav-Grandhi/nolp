c71b00591b297d9ac2973d7090fa1485b768c5c5
Pranav-Grandhi <pranav@grandhi.net>
2021-12-13 17:27:10 +0000

    navbar fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 
 export default function Header() {
+  const { data: session, status } = useSession()
+
   return (
     <>
       <header className="relative py-3 z-20 shadow">
@@ -23,45 +25,58 @@ export default function Header() {
               style={{ width: 'fit-content' }}
             >
               <ul className="flex justify-center">
-                <li>
-                  <Link href="/about">
-                    <a className="block px-5 py-3.5 text-sm font-medium text-black hover:text-red-500 focus:text-red-500 focus:outline-none focus-visible:shadow-none">
-                      About
-                    </a>
-                  </Link>
-                </li>
+                <input
+                  placeholder="Search ..."
+                  className="block px-5 py-3.5 text-sm font-medium text-black focus:outline-none focus-visible:shadow-none"
+                />
               </ul>
             </div>
             <div className="hidden lg:flex items-center justify-end -mr-4">
-              <button
-                className="flex items-center px-4 md:px-5 py-3.5 text-sm font-medium text-black hover:text-red-500 focus:text-red-500 focus:outline-none focus-visible:shadow-none"
-                onClick={() => signIn('google')}
-              >
-                Sign in
-              </button>
-              <button
-                className="flex items-center px-4 md:px-5 py-3.5 text-sm font-medium text-black hover:text-red-500 focus:text-red-500 focus:outline-none focus-visible:shadow-none"
-                onClick={() => signIn('google')}
-              >
-                Sign up
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="fill-current ml-1.5 -mr-0.5"
-                >
-                  <path
-                    clipRule="evenodd"
-                    d="M9.53 4.47L14.06 9l-4.53 4.53-1.06-1.06L12.14 9 8.47 5.53l1.06-1.06z"
-                  ></path>
-                  <path
-                    clipRule="evenodd"
-                    d="M3 8.25l9.5.1v1.3l-9.5.1v-1.5z"
-                  ></path>
-                </svg>
-              </button>
+              {status !== 'loading' && !session ? (
+                <>
+                  <button
+                    className="flex items-center px-4 md:px-5 py-3.5 text-sm font-medium text-black hover:text-red-500 focus:text-red-500 focus:outline-none focus-visible:shadow-none"
+                    onClick={() => signIn('google')}
+                  >
+                    Sign in
+                  </button>
+                  <button
+                    className="flex items-center px-4 md:px-5 py-3.5 text-sm font-medium text-black hover:text-red-500 focus:text-red-500 focus:outline-none focus-visible:shadow-none"
+                    onClick={() => signIn('google')}
+                  >
+                    Sign up
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="fill-current ml-1.5 -mr-0.5"
+                    >
+                      <path
+                        clipRule="evenodd"
+                        d="M9.53 4.47L14.06 9l-4.53 4.53-1.06-1.06L12.14 9 8.47 5.53l1.06-1.06z"
+                      ></path>
+                      <path
+                        clipRule="evenodd"
+                        d="M3 8.25l9.5.1v1.3l-9.5.1v-1.5z"
+                      ></path>
+                    </svg>
+                  </button>
+                </>
+              ) : (
+                <>
+                  <div className="flex items-center">
+                    <p className="text-sm font-medium text-black mr-5">
+                      {session?.user.name}
+                    </p>
+                    <img
+                      src={session?.user.image}
+                      className="w-7 h-7 rounded-full"
+                    />
+                  </div>
+                </>
+              )}
             </div>
             <button className="absolute right-0 w-10 h-10 lg:hidden">
               <svg
