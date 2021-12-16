32a4418657d60d91497074d1f1753209b93ff2cb
Agastya Gaur <agastya.gaur@gmail.com>
2021-12-16 18:51:12 +0000

    Video Player Added

--- src/components/vars.tsx
+++ src/components/vars.tsx
@@ -1,9 +1,14 @@
+import ReactPlayer from "react-player"
+
 export var LogoColor: string = '#EF4444' // To use, import { LogoColor } from './vars'
 
 export function NeverGonnaGiveYouUp() {
   return (
     <>
-      <p>Never Gonna Give You Up</p>
+      <ReactPlayer 
+        url='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        playing={true}
+      />
     </>
   )
 }
